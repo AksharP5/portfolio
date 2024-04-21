b027e80470101e2141bd36870729205225b45eb6
Akshar Patel <aksharcommit@gmail.com>
2024-04-21 18:00:11 -0400

    refactor: website logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-function App() {
+import React from 'react';
 
-
+const App = () => {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Coming Soon
     </h1>
-  )
-}
+  );
+};
 
-export default App
+export default App;
