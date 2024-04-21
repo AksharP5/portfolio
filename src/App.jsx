7f789c08b2afff914f6dd202c0de7faa8f0c1906
Akshar Patel <aksharpatel1233@gmail.com>
2024-04-21 22:21:05 +0000

    feat: Add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Layout from './components/Layout/Layout'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Coming Soon
-    </h1>
+    <>
+    <h1 className="text-3xl font-bold underline">Coming Soon</h1>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index  />
+        <Route path="about"  />
+        <Route path="contact"  />
+      </Route>
+    </Routes>
+    </>
   );
 };
 
