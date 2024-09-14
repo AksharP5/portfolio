04bf9a5193537801141e4ca1ba64786fdcf8705c
Akshar Patel <aksharcommit@gmail.com>
2024-09-13 23:06:47 -0400

    feat: add js for menu icon
    Add JavaScript for the menu icon to work properly
    
    ---
    
    * Closes #12
    
    ---

---
+++ src/script.js
@@ -0,0 +1,35 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll(".navbar a");
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove("active");
+                document.querySelector("nav a[href*=" + id + "]" ).classList.add("active")
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+}
+
+navLinks.forEach(link => {
+    link.onclick = () => {
+        navLinks.forEach(l => l.classList.remove("active"));
+        link.classList.add("active");
+        navbar.classList.remove("active");
+        menuIcon.classList.remove("bx-x");
+    };
+});
